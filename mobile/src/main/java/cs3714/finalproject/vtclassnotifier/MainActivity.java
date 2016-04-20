dbb927819f7f054e38b6eceae06c779d299604fd
Michael Maino <michaelmaino@Michaels-MacBook-Pro-6.local>
2016-04-20 12:34:57 -0400

    added buttons to main activity for navigation

--- mobile/src/main/java/cs3714/finalproject/vtclassnotifier/MainActivity.java
+++ mobile/src/main/java/cs3714/finalproject/vtclassnotifier/MainActivity.java
@@ -1,13 +1,50 @@
 package cs3714.finalproject.vtclassnotifier;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button submit;
+    Button classCart;
+    Button searchClasses;
+    Button options;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submit = (Button) findViewById(R.id.submitButton);
+        classCart = (Button) findViewById(R.id.cart);
+        searchClasses = (Button) findViewById(R.id.searchClasses);
+        options = (Button) findViewById(R.id.options);
+        submit.setOnClickListener(this);
+        classCart.setOnClickListener(this);
+        searchClasses.setOnClickListener(this);
+        options.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == submit.getId()) {
+
+        } else if (v.getId() == classCart.getId()) {
+
+        } else if (v.getId() == searchClasses.getId()) {
+            Intent searchClass = new Intent(this, SearchClasses.class);
+            startActivity(searchClass);
+
+        } else if (v.getId() == options.getId()) {
+
+        }
+
+
     }
 }
