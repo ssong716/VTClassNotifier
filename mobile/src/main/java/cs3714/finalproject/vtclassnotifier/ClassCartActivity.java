a7beb15f4ba502f10f9b705c06643fdb034f462a
Michael Maino <michaelmaino@Michaels-MacBook-Pro-6.local>
2016-04-24 17:16:21 -0400

    Added a class cart activity. Also made a couple changes to the search classes activity

---
+++ mobile/src/main/java/cs3714/finalproject/vtclassnotifier/ClassCartActivity.java
@@ -0,0 +1,13 @@
+package cs3714.finalproject.vtclassnotifier;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClassCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_cart);
+    }
+}
